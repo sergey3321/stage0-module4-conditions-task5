5c6fcca1fbf4013016fa6114c8f376851b12980e
Sergey234 <mostov.sergey88@gmail.com>
2022-11-10 15:30:20 +0300

    stage0-module4-conditions-task5

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -1,6 +1,11 @@
 package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
-    public void isLeapYear(int year) {
+    public static void isLeapYear(int year) {
+        if (year % 4 == 0) {
+            System.out.println("leap");
+        } else {
+            System.out.println("not leap");
+        }
     }
 }
